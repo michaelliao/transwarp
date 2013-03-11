f981871efb8b8cfe7655f0a35e6a6dfa5d054d47
Michael Liao <askxuefeng@gmail.com>
2013-03-11 10:51:11 +0800

    add testcase

--- transwarp/db.py
+++ transwarp/db.py
@@ -250,6 +250,24 @@ def transaction():
 
     with transaction():
         pass
+
+    >>> def update_profile(id, name, rollback):
+    ...     u = dict(id=id, name=name, email='%s@test.org' % name, passwd=name, last_modified=time.time())
+    ...     insert('user', **u)
+    ...     r = update('update user set passwd=? where id=?', name.upper(), id)
+    ...     if rollback:
+    ...         raise StandardError('will cause rollback...')
+    >>> with transaction():
+    ...     update_profile(900301, 'Python', False)
+    >>> select_one('select * from user where id=?', 900301).name
+    u'Python'
+    >>> with transaction():
+    ...     update_profile(900302, 'Ruby', True)
+    Traceback (most recent call last):
+      ...
+    StandardError: will cause rollback...
+    >>> select('select * from user where id=?', 900302)
+    []
     '''
     return _Transaction()
 
