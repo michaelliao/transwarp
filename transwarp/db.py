2fee642418a3c0b7d74d2aad7a4f671c4db27f29
Michael Liao <askxuefeng@gmail.com>
2013-06-14 15:32:31 +0800

    add timestamp in next_str()

--- transwarp/db.py
+++ transwarp/db.py
@@ -49,8 +49,16 @@ _id_generator = None
 def next_int():
     return _id_generator.next_id()
 
-def next_str():
-    return '%018d%s' % (int(time.time()*100000), uuid.uuid4().hex)
+def next_str(t=None):
+    '''
+    Return next id as 50-char string.
+
+    Args:
+        t: unix timestamp, default to None and using time.time().
+    '''
+    if t is None:
+        t = time.time()
+    return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
 
 class Dict(dict):
     '''
