44ee0d394d94a3b13b68b3f3c9ae6069a47c09ab
Michael Liao <askxuefeng@gmail.com>
2013-08-27 11:29:37 +0800

    move UTC class from web to utils module

--- transwarp/utils.py
+++ transwarp/utils.py
@@ -7,6 +7,8 @@ __author__ = 'Michael Liao'
 Utils
 '''
 
+import re, datetime
+
 class Dict(dict):
     '''
     Simple dict but support access as x.y style.
@@ -50,6 +52,67 @@ class Dict(dict):
 
     def __setattr__(self, key, value):
         self[key] = value
+
+_TIMEDELTA_ZERO = datetime.timedelta(0)
+
+# timezone as UTC+8:00, UTC-10:00
+_RE_TZ = re.compile('^([\+\-])([0-9]{1,2})\:([0-9]{1,2})$')
+
+class UTC(datetime.tzinfo):
+    '''
+    A UTC tzinfo object. 
+
+    >>> tz0 = UTC('+00:00')
+    >>> tz0.tzname(None)
+    'UTC+00:00'
+    >>> tz8 = UTC('+8:00')
+    >>> tz8.tzname(None)
+    'UTC+8:00'
+    >>> tz7 = UTC('+7:30')
+    >>> tz7.tzname(None)
+    'UTC+7:30'
+    >>> tz5 = UTC('-05:30')
+    >>> tz5.tzname(None)
+    'UTC-05:30'
+    >>> from datetime import datetime
+    >>> u = datetime.utcnow().replace(tzinfo=tz0)
+    >>> l1 = u.astimezone(tz8)
+    >>> l2 = u.replace(tzinfo=tz8)
+    >>> d1 = u - l1
+    >>> d2 = u - l2
+    >>> d1.seconds
+    0
+    >>> d2.seconds
+    28800
+    '''
+
+    def __init__(self, utc):
+        utc = str(utc.strip().upper())
+        mt = _RE_TZ.match(utc)
+        if mt:
+            minus = mt.group(1)=='-'
+            h = int(mt.group(2))
+            m = int(mt.group(3))
+            if minus:
+                h, m = (-h), (-m)
+            self._utcoffset = datetime.timedelta(hours=h, minutes=m)
+            self._tzname = 'UTC%s' % utc
+        else:
+            raise ValueError('bad utc time zone')
+
+    def utcoffset(self, dt):
+        return self._utcoffset
+
+    def dst(self, dt):
+        return _TIMEDELTA_ZERO
+
+    def tzname(self, dt):
+        return self._tzname
+
+    def __str__(self):
+        return 'UTC tzinfo object (%s)' % self._tzname
+
+    __repr__ = __str__
 
 def load_module(modname):
     '''
--- transwarp/web.py
+++ transwarp/web.py
@@ -9,9 +9,9 @@ __author__ = 'Michael Liao'
 
 import __builtin__
 
-import types, sys, os, re, cgi, sys, base64, json, time, hashlib, inspect, datetime, functools, mimetypes, threading, logging, urllib, collections, linecache
+import types, os, re, cgi, sys, base64, time, datetime, inspect, functools, mimetypes, threading, logging, urllib, collections, linecache
 
-from utils import Dict, load_module
+from utils import Dict, UTC, load_module
 
 # thread local object for storing request and response.
 ctx = threading.local()
@@ -834,66 +834,6 @@ class Request(object):
         u'DEFAULT'
         '''
         return self.cookies.get(name, default)
-
-_TIMEDELTA_ZERO = datetime.timedelta(0)
-
-_RE_TZ = re.compile('^([\+\-])([0-9]{1,2})\:([0-9]{1,2})$')
-
-class UTC(datetime.tzinfo):
-    '''
-    A UTC tzinfo object. 
-
-    >>> tz0 = UTC('+00:00')
-    >>> tz0.tzname(None)
-    'UTC+00:00'
-    >>> tz8 = UTC('+8:00')
-    >>> tz8.tzname(None)
-    'UTC+8:00'
-    >>> tz7 = UTC('+7:30')
-    >>> tz7.tzname(None)
-    'UTC+7:30'
-    >>> tz5 = UTC('-05:30')
-    >>> tz5.tzname(None)
-    'UTC-05:30'
-    >>> from datetime import datetime
-    >>> u = datetime.utcnow().replace(tzinfo=tz0)
-    >>> l1 = u.astimezone(tz8)
-    >>> l2 = u.replace(tzinfo=tz8)
-    >>> d1 = u - l1
-    >>> d2 = u - l2
-    >>> d1.seconds
-    0
-    >>> d2.seconds
-    28800
-    '''
-
-    def __init__(self, utc):
-        utc = str(utc.strip().upper())
-        mt = _RE_TZ.match(utc)
-        if mt:
-            minus = mt.group(1)=='-'
-            h = int(mt.group(2))
-            m = int(mt.group(3))
-            if minus:
-                h, m = (-h), (-m)
-            self._utcoffset = datetime.timedelta(hours=h, minutes=m)
-            self._tzname = 'UTC%s' % utc
-        else:
-            raise ValueError('bad utc time zone')
-
-    def utcoffset(self, dt):
-        return self._utcoffset
-
-    def dst(self, dt):
-        return _TIMEDELTA_ZERO
-
-    def tzname(self, dt):
-        return self._tzname
-
-    def __str__(self):
-        return 'UTC tzinfo object (%s)' % self._tzname
-
-    __repr__ = __str__
 
 UTC_0 = UTC('+00:00')
 
